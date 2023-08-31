7b127be291d6578ec341126c51d5895f5bb8fbe6
yesseruser <yesseruseryt@gmail.com>
2023-08-31 21:45:32 +0200

    Added code files.

---
+++ src/PackageUpdater_yesseruser/__init__.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def updatePackages():
+    print("Getting outdated pip packages...")
+    # Run pip list command to get the outdated packages
+    outdated_packages = subprocess.check_output(['pip', 'list', '--outdated']).decode('utf-8')
+
+    # Split the output into lines and ignore the header
+    lines = outdated_packages.strip().split('\n')[2:]
+
+    print("Updating packages using pip...")
+    # Update each package
+    for line in lines:
+        package_info = line.split()
+        package_name = package_info[0]
+        subprocess.call(['python', '-m', 'pip', 'install', '--upgrade', package_name])
+
+    print("All outdated packages have been updated. Thank you for using this package.")
