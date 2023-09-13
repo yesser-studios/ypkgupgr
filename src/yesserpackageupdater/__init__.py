af1961da56dbfe45cf8fd177848bc46e28c86a67
yesseruser <yesseruseryt@gmail.com>
2023-09-13 11:13:49 +0200

    Made the code OS independant.

--- src/yesserpackageupdater/__init__.py
+++ src/yesserpackageupdater/__init__.py
@@ -1,9 +1,10 @@
 import subprocess
+import sys
 
 def updatePackages():
     print("Getting outdated pip packages...")
     # Run pip list command to get the outdated packages
-    outdated_packages = subprocess.check_output(['python', '-m', 'pip', 'list', '--outdated']).decode('utf-8')
+    outdated_packages = subprocess.check_output([sys.executable, '-m', 'pip', 'list', '--outdated']).decode('utf-8')
 
     # Split the output into lines and ignore the header
     lines = outdated_packages.strip().split('\n')[2:]
@@ -17,7 +18,7 @@ def updatePackages():
     for line in lines:
         package_info = line.split()
         package_name = package_info[0]
-        subprocess.call(['python', '-m', 'pip', 'install', '--upgrade', package_name])
+        subprocess.call([sys.executable, '-m', 'pip', 'install', '--upgrade', package_name])
 
     print("All outdated packages have been updated. Thank you for using this package.")
 
