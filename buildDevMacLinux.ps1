aa995e93729ae31bb95f16961d1ad83dcd12dab6
yesseruser <yesseruseryt@gmail.com>
2023-09-15 21:21:09 +0200

    Added a command to upgrade twine to the PS scripts.

--- buildDevMacLinux.ps1
+++ buildDevMacLinux.ps1
@@ -1,5 +1,6 @@
 python3 -m pip install --upgrade pip
 pip3 install --upgrade build
+pip3 install --upgrade twine
 Remove-Item "dist" -Recurse
 python3 -m build
 python3 -m twine upload dist/*
--- buildDevWindows.ps1
+++ buildDevWindows.ps1
@@ -1,5 +1,6 @@
 python -m pip install --upgrade pip
 pip install --upgrade build
+pip install --upgrade twine
 Remove-Item "dist" -Recurse
 python -m build
 twine upload dist/*
