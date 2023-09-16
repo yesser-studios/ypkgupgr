32a974fc2b1b47d60e1a05099d0562ef04a0fc11
yesseruser <yesseruseryt@gmail.com>
2023-09-16 12:30:58 +0200

    Added a global definition at the start of update.

--- src/yesserpackageupdater/__init__.py
+++ src/yesserpackageupdater/__init__.py
@@ -5,6 +5,8 @@ import asyncio
 failed = ""
 
 async def update(name: str):
+    global failed
+
     # subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", name])
     process = await asyncio.create_subprocess_shell(sys.executable + " -m pip install --upgrade " + name,
                                                     stdout=asyncio.subprocess.PIPE,
