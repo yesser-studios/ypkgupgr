84d553fc9f7ecc13cf450923f49eecc0c085832d
yesseruser <yesseruseryt@gmail.com>
2023-10-17 17:31:22 +0200

    Added logs.
    Signed-off-by: yesseruser <yesseruseryt@gmail.com>

--- src/yesserpackageupdater/__init__.py
+++ src/yesserpackageupdater/__init__.py
@@ -37,6 +37,7 @@ def progress_ring(progress, complete = False, intermediate = False):
 
     # Checks if the user is on Windows. Otherwise the progress string might be printed and would confuse the user on Linux and Mac.
     if sys.platform != "win32":
+        logger.debug("Progress ring not shown because platform is not Windows.")
         return
     
     # Gets the correct progress ring state.
@@ -52,6 +53,8 @@ def progress_ring(progress, complete = False, intermediate = False):
     
     # Prints the progress string according to https://github.com/MicrosoftDocs/terminal/blob/main/TerminalDocs/tutorials/progress-bar-sequences.md
     print(f"{chr(27)}]9;4;{state};{progress}{chr(7)}", end="")
+
+    logger.debug(f"Progress ring updated with the following data: State: {state}; Progress: {progress}")
         
 
 async def update(name: str):
@@ -64,6 +67,8 @@ async def update(name: str):
     global finished_count
     global yesserpackageupdater_outdated
 
+    logger.info(f"Updating {name}")
+
     # Checks if the user is on windows and this package is updated using the script. Fixes issue #11 (https://github.com/yesseruser/YesserPackageUpdater/issues/11).
     if name == "yesserpackageupdater" and ran_from_script and sys.platform == "win32":
         yesserpackageupdater_outdated = True
@@ -71,11 +76,14 @@ async def update(name: str):
         progress = int((finished_count / outdated_count) * 100)
         progress_ring(progress)
         print("yesserpackageupdater is outdated and you are using the script. Continuing to update other packages... ({progress}% - {finished_count}/{outdated_count} complete or failed)")
+        logger.info(f"Skipping yesserpackageupdater. See bottom of the logs for details.")
 
         if failed == "":
             failed = name
         else:
             failed += ", " + name
+
+        logger.debug("Added yesserpackageupdater into failed.")
 
         return
 
@@ -84,8 +92,12 @@ async def update(name: str):
                                                     stdout=asyncio.subprocess.PIPE,
                                                     stderr=asyncio.subprocess.PIPE)
     
+    logger.debug("Subprocess shell created.")
+    
     # Gets this process' return code.
     return_code = await process.wait()
+
+    logger.debug(f"Subprocess ended with code {return_code}")
     
     # Adds a finished and updates the progress ring.
     finished_count += 1
@@ -97,11 +109,17 @@ async def update(name: str):
     # Checks for update success and if failed, logs the package's name into the failed list.
     if return_code == 0:
         print(f"Successfully updated {name} ({progress}% - {finished_count}/{outdated_count} complete or failed)")
+        logger.info(f"Successfully updated {name}.")
     else:
+        logger.error(f"{name} failed to update; below is pip output:")
+
         # Separates the output string by lines.
         (out, err) = await process.communicate()
         for line in out.strip().decode().splitlines():
             print(line)
+            logger.error(line)
+
+        logger.info("End of pip output.")
         
         print(f"{name} failed to update. ({progress}% - {finished_count}/{outdated_count} complete or failed)")
         
@@ -109,6 +127,8 @@ async def update(name: str):
             failed = name
         else:
             failed += ", " + name
+
+        logger.debug(f"Added {name} to failed.")
 
 def update_packages():
     """
@@ -118,12 +138,12 @@ def update_packages():
     global outdated_count
     global yesserpackageupdater_outdated
 
-    logger.info("Starting update.")
+    logger.info(f"Starting update. Platform: {sys.platform}")
     
     progress_ring(progress = 0, intermediate = True)
 
     print("Getting outdated pip packages...")
-    logger.info("Getting outdated pip packages")
+    logger.info("Getting outdated packages.")
 
     # Runs the pip list --outdated command to get the outdated packages.
     outdated_packages = subprocess.check_output([sys.executable, '-m', 'pip', 'list', '--outdated']).decode('utf-8')
@@ -135,17 +155,25 @@ def update_packages():
     if len(lines) <= 0:
         progress_ring(progress = 100, complete = True)
         print("No outdated packages found.")
+        logger.info("No outdated packages.")
         return
     
     outdated_count = len(lines)
 
+    logger.info(f"Outdated packages: {lines}")
+
     print("Updating packages using pip...")
+
+    logger.info("Starting to update packages...")
 
     # Updates each package
     for line in lines:
         package_info = line.split()
         package_name = package_info[0]
+        logger.debug(f"Starting to update {package_name}")
         asyncio.run(update(package_name))
+
+    logger.info("Finished updating packages.")
 
     # Empty line before conclusion.
     print()
@@ -153,12 +181,15 @@ def update_packages():
     # Prints conclusion.
     if len(failed) == 0:
         print("All outdated packages have been updated. Thank you for using this package.")
+        logger.info(f"All outdated packages updated with a total of {outdated_count} packages.")
     else:
         print("The following packages failed to install: " + failed + "\n")
+        logger.info(f"The following packages failed to install: {failed}")
 
     # Warns the user if yesserpackageupdater hasn't been updated.
     if yesserpackageupdater_outdated:
         print(f'The yesserpackageupdater package is outdated, and you are using the script. Please use "{sys.executable} -m yesserpackageupdater" to update it.\n')
+        logger.info("yesserpackageupdater is outdated and the script is used on Windows.")
     
     progress_ring(progress = 100,complete = True)
 
@@ -172,6 +203,6 @@ def update_packages_script():
     ran_from_script = True
     init_logging()
 
-    logger.info("Starting from script.")
+    logger.info("Starting from script...")
 
     update_packages()
