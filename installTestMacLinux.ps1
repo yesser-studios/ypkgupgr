f09af64abda466fdf4ffcbf3b86cdd738a02b179
yesseruser <yesseruseryt@gmail.com>
2023-10-17 17:10:12 +0200

    The install scripts now don't install from the internet.

--- installTestMacLinux.ps1
+++ installTestMacLinux.ps1
@@ -2,4 +2,4 @@ python3 -m pip install --upgrade pip
 pip3 install --upgrade build
 Remove-Item "dist" -Recurse
 python3 -m build
-python3 -m pip install --force-reinstall --find-links=./dist yesserpackageupdater
+python3 -m pip install --force-reinstall --no-index --find-links=./dist yesserpackageupdater
--- installTestWindows.ps1
+++ installTestWindows.ps1
@@ -2,4 +2,4 @@ python -m pip install --upgrade pip
 pip install --upgrade build
 Remove-Item "dist" -Recurse
 python -m build
-python -m pip install --force-reinstall --find-links=.\dist yesserpackageupdater
+python -m pip install --force-reinstall --no-index --find-links=.\dist yesserpackageupdater
