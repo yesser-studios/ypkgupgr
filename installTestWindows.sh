f01c63c9625ae946e332a6806340ac05a48ca394
yesseruser <yesseruseryt@gmail.com>
2023-11-22 20:56:45 +0100

    Made them sh scripts.
    Signed-off-by: yesseruser <yesseruseryt@gmail.com>

--- installTestWindows.sh
+++ installTestWindows.sh
@@ -1,5 +1,5 @@
 python -m pip install --upgrade pip
 pip install --upgrade build
-Remove-Item "dist" -Recurse
+rm -r dist
 python -m build
 python -m pip install --force-reinstall --no-index --find-links=.\dist yesserpackageupdater
