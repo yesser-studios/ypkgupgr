d4d36f959363f068be3868c23686b03ffb6ee8e9
yesseruser <yesseruseryt@gmail.com>
2023-11-22 21:12:40 +0100

    Made the windows script a ps script.
    Signed-off-by: yesseruser <yesseruseryt@gmail.com>

--- installTestWindows.ps1
+++ installTestWindows.ps1
@@ -1,5 +1,5 @@
 python -m pip install --upgrade pip
 pip install --upgrade build
-rm -r dist
+Remove-Item "dist" -Recurse
 python -m build
 python -m pip install --force-reinstall --no-index --find-links=.\dist yesserpackageupdater
