7af5f9f15d91b00b93b0157a72120eecc77f6473
yesseruser <yesseruseryt@gmail.com>
2023-11-26 15:11:11 +0100

    Added type notation.

--- src/yesserpackageupdater/colors.py
+++ src/yesserpackageupdater/colors.py
@@ -1,5 +1,5 @@
 class Colors:
-    WHITE = '\u001b[37m'
-    RED = '\u001b[31m'
-    YELLOW = '\u001b[33m'
-    GREEN = '\u001b[32m'
+    WHITE: str = '\u001b[37m'
+    RED: str = '\u001b[31m'
+    YELLOW: str = '\u001b[33m'
+    GREEN: str = '\u001b[32m'
