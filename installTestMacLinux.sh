5785f63485a9c2504815418cf51510e0e1f55db2
yesseruser <yesseruseryt@gmail.com>
2023-12-03 10:56:52 +0100

    Updated install scripts.

--- installTestMacLinux.sh
+++ installTestMacLinux.sh
@@ -2,4 +2,4 @@ python3 -m pip install --upgrade pip
 pip3 install --upgrade build
 rm -r ./dist
 python3 -m build
-python3 -m pip install --force-reinstall --no-index --find-links=./dist yesserpackageupdater
+python3 -m pip install --force-reinstall --no-index --find-links=./dist ypkgupgr
