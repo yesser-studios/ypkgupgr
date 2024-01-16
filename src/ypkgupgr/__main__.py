f4d3a5f98a1b5546402a48275624fd4b7c81452c
yesseruser <yesseruseryt@gmail.com>
2024-01-16 17:37:41 +0100

    Made the module import from the new files.
    Signed-off-by: yesseruser <yesseruseryt@gmail.com>

--- src/ypkgupgr/__main__.py
+++ src/ypkgupgr/__main__.py
@@ -1,4 +1,5 @@
-from .old_init import update_packages, init_logging, create_appdata_dirs
+from . import update_packages, init_logging
+from .appdata import create_appdata_dirs
 
 create_appdata_dirs()
 init_logging()
